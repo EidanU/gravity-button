725178173455415eb01de5de5cc28b1083cbf41a
EidanU <eidanughetti.contact@gmail.com>
2023-07-09 01:02:03 +0200

    second version

--- index.js
+++ index.js
@@ -9,13 +9,11 @@ let initRelativeButtonPosition =
   gravityZoneElement.offsetHeight / 2 - buttonElement.offsetHeight / 2;
 let interval;
 let angle;
+let hyp;
 
 gravityZoneElement.addEventListener("mouseenter", calcultateMousePosition);
 gravityZoneElement.addEventListener("mouseleave", () => {
-  let marginleft = initRelativeButtonPosition;
-  let marginTop = initRelativeButtonPosition;
-  buttonElement.style.left = `${marginleft}px`;
-  buttonElement.style.top = `${marginTop}px`;
+  buttonElement.style.transform = `translate(0% ,0%)`;
   clearInterval(interval);
 });
 
@@ -26,38 +24,52 @@ window.addEventListener("mousemove", (event) => {
 
 function calcultateMousePosition() {
   interval = setInterval(() => {
+    // On recuperer la position en X de la souris par rapport au contenu, comme ca le 0 se situe au milieu de ce dernier
     relativeMousePositionX =
       mousePositionX -
       gravityZoneElement.getBoundingClientRect().left -
       gravityZoneElement.offsetWidth / 2;
 
+    // On recuperer la position en X de la souris par rapport au contenu, comme ca le 0 se situe au milieu de ce dernier
     relativeMousePositionY =
       mousePositionY -
       gravityZoneElement.getBoundingClientRect().top -
       gravityZoneElement.offsetHeight / 2;
 
+    // On recupere l'angle de la souri juste pour pouvoir avoir le cos/sin à la fin
     angle = Math.atan2(relativeMousePositionY, relativeMousePositionX);
+    let cos = Math.cos(angle);
+    let sin = Math.sin(angle);
 
-    let coefX = Math.round(
-      (gravityZoneElement.offsetWidth / 2 - Math.abs(relativeMousePositionX)) *
-        Math.cos(angle) *
-        0.5
-    );
-    let coefY = Math.round(
-      (gravityZoneElement.offsetHeight / 2 - Math.abs(relativeMousePositionY)) *
-        Math.sin(angle) *
-        0.5
+    // On recupere l'hyp de la souri juste pour pouvoir avoir le cos/sin à la fin
+    hyp = Math.sqrt(
+      relativeMousePositionX * relativeMousePositionX +
+        relativeMousePositionY * relativeMousePositionY
     );
 
-    if (coefX > 0 && coefY > 0) {
+    //15 / 50 × 100 = 30%
+    // il faut que plus la distance entre la souris et le bouton diminue, plus le translate soit fort
+    // d'ou le (gravityZoneElement.offsetWidth / 2 - Math.abs(relativeMousePositionX)) qui équivaut à une distance d'equart
+
+    let translateX = Math.round(
+      ((gravityZoneElement.offsetWidth / 2 - Math.abs(relativeMousePositionX)) /
+        (gravityZoneElement.offsetWidth / 2)) *
+        100
+    );
+    let translateY = Math.round(
+      ((gravityZoneElement.offsetWidth / 2 - Math.abs(relativeMousePositionY)) /
+        (gravityZoneElement.offsetWidth / 2)) *
+        100
+    );
+
+    translateX *= cos * 0.5;
+    translateY *= sin * 0.5;
+    if (translateX > 80 && translateY > 80) {
+      translateX =
+        (relativeMousePositionX / (gravityZoneElement.offsetWidth / 2)) * 100;
+      translateY =
+        (relativeMousePositionY / (gravityZoneElement.offsetWidth / 2)) * 100;
     }
-    let marginleft = initRelativeButtonPosition + coefX;
-    let marginTop = initRelativeButtonPosition + coefY;
-
-    buttonElement.style.left = `${marginleft}px`;
-    buttonElement.style.top = `${marginTop}px`;
+    buttonElement.style.transform = `translate(${translateX}% ,${translateY}%)`;
   }, 1000 / 120);
 }
-
-// Qu'est ce que je retiens
-// Pour trouver une proportionnalité inversée par rapport à un hyp, il suffit juste de prendre l'espace total et de le soustraire
